44b521b5a812ecefa498c701680f3ac2240cd38b
Erick Pinos <erick@erickpinos.com>
2023-10-23 19:24:37 +0100

    updated website

--- src/App.js
+++ src/App.js
@@ -7,61 +7,65 @@ function App() {
     <div className="App">
       <header className="App-header">
           Erick Pinos
+          <h4>Ecosystem Growth Lead @ Nibiru | MIT 🦫</h4>
       </header>
 
-      <div class="content container">
+      <div className="content container">
 
-      <div class="row mt-5 mb-5 p-1">
-        <div class="col-12 text-center">
+      <div className="row mt-5 mb-5 p-1">
+        <div className="col-12 text-center">
           <img src={consensus2019}
             alt="consensus-2019"
-            style={{width: 'auto', maxWidth: '100%'}} />
+            style={{width: 'auto', maxWidth: '50%'}} />
         </div>
       </div>
 
-      <div class="row mt-5 mb-5">
-        <div class="col-12 text-center">
-          <h2>Erick's Mission</h2>
-        </div>
-        <div class="col-12">
-          Using the technology of the future, including blockchain, virtual reality, to make a socio-economic impact.
-          Growing open-source technology and open-source communities centered around innovation.
+      <div className="row mt-5 mb-5 p-1">
+        <div className="col-12 text-center">
+          <h2>Follow Me</h2>
+          <p>Stay updated with insights on AI and metaverse startups.</p>
+          <a href="https://twitter.com/erickpinos">Twitter</a> | <a href="https://www.linkedin.com/in/erickpinos/">LinkedIn</a>
         </div>
       </div>
 
-      <div class="row mt-5 mb-5">
-        <div class="col-12 text-center">
-          <h2>Bio</h2>
+      <div className="row mt-5 mb-5">
+        <div className="col-12 text-center">
+          <h2>About Erick</h2>
         </div>
-        <div class="col-12">
-        <p>
-          Erick Pinos is the Americas Ecosystem Lead at Ontology Network, spearheading blockchain-based decentralized identity solutions for both individuals and enterprises. He has also been the President of BEN (Blockchain Education Network) for two years, onboarding dozens of new student-run blockchain clubs and speaking at various conferences about blockchain education.
-        </p>
-        <p>
-          He holds a degree in Management from MIT with a concentration in Entrepreneurship. At MIT, Erick was the President of the MIT Bitcoin Club and Co-Director of the 2018 MIT Bitcoin Expo. Erick was also a researcher at the MIT Digital Currency Initiative, designing tokenized renewable energy solar microgrids. After graduating, he did an internship on Nasdaq’s blockchain team and technical due diligence for the crypto investment firm Game Theory Group.
-        </p>
-        </div>
-      </div>
-
-      <div class="row mt-5 mb-5">
-        <div class="col-12 text-center">
-          <h2>Podcast Appearances</h2>
-        </div>
-        <div class="col-12 text-center">
-          <div><a href="https://www.youtube.com/watch?v=6Cr0UiSILHA">Tech Talks Daily Podcast Interview (by Neil Hughes)</a></div>
-          <div><a href="https://thegsm.co/2020/05/bitcoin-halving-and-a-pulse-check-of-the-global-blockchain-ecosystem/">Global Startup Movement Podcast Interview (Andrew Berkowitz)</a></div>
-          <div><a href="https://podcasts.apple.com/gb/podcast/how-blockchain-will-change-academia-and-education/id1476856904?i=1000478790696">DIVI Crypto Podcast Interview (by Steve McGarry)</a></div>
+        <div className="col-12">
+          <p>
+            Erick Pinos is the Ecosystem Growth Lead at Nibiru and the President of BEN. He's an MIT graduate with a passion for the metaverse and AI. He has been an angel investor in various startups and tech initiatives.
+          </p>
+          <p>
+            With a mission to use futuristic technologies for socio-economic impact, Erick is dedicated to growing open-source technology and communities centered around innovation.
+          </p>
         </div>
       </div>
 
-      <div class="row mt-5 mb-5">
-        <div class="col-12 text-center">
-          <h2>Social Media</h2>
+      <div className="row mt-5 mb-5">
+        <div className="col-12 text-center">
+          <h2>Experience & Education</h2>
         </div>
-        <div class="col-12 text-center">
-        <span>
-          <a href="https://twitter.com">Twitter</a>
-        </span>
+        <div className="col-12">
+          <ul>
+            <li>Ecosystem Growth Lead at Nibiru Chain</li>
+            <li>President at BEN</li>
+            <li>Ecosystem Lead, Americas at Ontology</li>
+            <li>Lead Research Analyst at Game Theory Group</li>
+            <li>Bachelor of Science, Management from Massachusetts Institute of Technology</li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="row mt-5 mb-5">
+        <div className="col-12 text-center">
+          <h2>Recent Posts</h2>
+        </div>
+        <div className="col-12">
+          <ul>
+            <li><a href="https://www.linkedin.com/feed/update/urn:li:activity:7122267068883759105/">Why did YCombinator introduce SAFEs back in 2013?</a></li>
+            <li><a href="https://www.linkedin.com/feed/update/urn:li:activity:7121843328312119296/">What's the difference between a Convertible note and a SAFE?</a></li>
+          </ul>
         </div>
       </div>
 
